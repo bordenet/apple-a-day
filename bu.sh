8da361580de49fd36f3d1b1cafa0a41a97b793e1
Matt J Bordenet <bordenet@users.noreply.github.com>
2022-08-18 19:13:42 -0700

    Create bu.sh
    Initial commit so relatively less experienced folks can copy and extend this simple script

---
+++ bu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# I save this across my Macs as "bu.sh" bc it's easy to type. This script originated as a shorthand for "brew update"
+#
+brew update
+brew upgrade
+brew cleanup -s
+brew upgrade --cask
+
+brew doctor
+brew missing
+apm upgrade -c false
+
+# bower update
+# npm update -g --force
+# npm install -g npm --force
+
+# mas outdated
+# echo "install with: mas upgrade"
+# mas upgrade
+
+# sudo -H pip install --upgrade pip
+# sudo -H pip3 install --upgrade pip
+
+# /Users/matt/GitHub/fetch-github-projects.sh
+
+sudo softwareupdate --all --install --force -R
